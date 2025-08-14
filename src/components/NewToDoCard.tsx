11aa07e24f2caa45c0aebc968bd8e07ce157bb24
NeitzelJulia <julia.neitzel86@gmail.com>
2025-08-14 10:39:08 +0200

    save new todo

--- src/components/NewToDoCard.tsx
+++ src/components/NewToDoCard.tsx
@@ -3,12 +3,28 @@ import styles from "./ToDo.module.css";
 import plusIcon from "../assets/icons/plus-circle-svgrepo-com.svg";
 import Modal from "./Modal.tsx";
 import ToDoForm from "./ToDoForm.tsx";
+import axios from "axios";
+
+type Status = "OPEN" | "IN_PROGRESS" | "DONE";
+type TodoPayload = { description: string; status: Status };
 
 export default function NewToDoCard(){
     const [open, setOpen] = useState(false);
 
     function openNewToDoDialog(){
         setOpen(true);
+    }
+
+    function saveNewTodo(data: TodoPayload) {
+        axios
+            .post("/api/todo", data)
+            .then((res) => {
+                console.log("Todo erstellt:", res.data);
+                setOpen(false); // Modal schließen
+            })
+            .catch((err) => {
+                console.error("Fehler beim Erstellen:", err);
+            });
     }
 
     return (
@@ -20,14 +36,12 @@ export default function NewToDoCard(){
             >
                 <img src={plusIcon} alt="Neues ToDo hinzufügen" className={styles.plusSvg} />
             </li>
-            <Modal open={open} onClose={() => setOpen(false)} title="Neues ToDo">
-                <h3>Neues ToDo erstellen</h3>
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <h3>Neues To-Do erstellen</h3>
                 <ToDoForm
                     initialDescription=""
                     initialStatus="OPEN"
-                    onSave={(data) => {
-                        console.log("Geändertes ToDo:", data);
-                    }}
+                    onSave={saveNewTodo}
                     saveLabel="Speichern"
                 />
             </Modal>
